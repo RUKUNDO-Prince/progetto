5040340e802274947f58196d10e9a23f32b1766f
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-05-31 08:41:41 +0300

    progetto

--- dashboard/dash.js
+++ dashboard/dash.js
@@ -1,9 +1,9 @@
 const handleNavigate = (way) => {
   // check if the way is tournament and navigate to tournament page
   if (way === "tournament") {
-    window.location.href = "../tournament/tournament.html";
+    window.location.href = "../tournament/tournament.php";
   } else {
-    // navigate to the champinoship page
-    window.location.href = "../championships/championship.html";
+    // navigathttp://localhost/progetto/dashboard/dash.jse to the champinoship page
+    window.location.href = "../championships/championship.php";
   }
 };
